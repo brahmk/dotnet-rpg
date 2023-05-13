dc2f93112972fd64675422728e674e16e27be4d7
brahmk <brahm.kelly@gmail.com>
2023-05-13 15:00:39 -0400

    building models and services

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+global using dotnet_rpg.Services.CharacterService;
+global using dotnet_rpg.Models;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
